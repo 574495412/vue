3b5845098359173db941e7b55a13ed847eddc088
huangwenjun <461421738@qq.com>
2018-06-23 09:18:59 +0800

    solve conflict

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,12 +1,9 @@
 <template>
   <div class="dashboard-container">
     <!-- 头部 -->
-<<<<<<< HEAD
       <head-top head-title="红包说明" go-back='true'></head-top>
       <leftNav></leftNav>
-=======
       <head-top></head-top>
->>>>>>> 4eb1d3e1913220ce47436478b256014542538bae
   </div>
 </template>
 <script>
@@ -23,23 +20,18 @@
       return {}
     },
     computed: {
-     
+
     },
-   
+
     methods: {
     }
   }
 </script>
 
 <style scoped lang="less">
-
-<<<<<<< HEAD
     &-box {
       font-size: 32px;
       margin: 32px auto;
     }
-  }
 </style>
-=======
-</style>
->>>>>>> 4eb1d3e1913220ce47436478b256014542538bae
+
